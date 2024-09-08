cb3495357fd990fa64f053ffd8c6f4f82552697d
sharvinegi <sharvinegi5703@gmail.com>
2024-09-09 05:16:02 +0530

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var text = "HIMANSHI SHARMA";
-    var typingAnimationElement = document.getElementById("typing-animation");
-    var index = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    const textElement = document.getElementById('typing-animation');
+    const phrases = [" developer", " fast learner", " problem solver"];
+    let index = 0;
+    let charIndex = 0;
+    let isTyping = true;
 
     function type() {
-        if (index < text.length) {
-            typingAnimationElement.innerHTML += text.charAt(index);
-            index++;
-            setTimeout(type, 150); 
+        if (isTyping) {
+            if (charIndex < phrases[index].length) {
+                textElement.textContent += phrases[index].charAt(charIndex);
+                charIndex++;
+                setTimeout(type, 100);
+            } else {
+                isTyping = false;
+                setTimeout(erase, 1000); // Pause before erasing
+            }
+        } else {
+            if (charIndex > 0) {
+                textElement.textContent = phrases[index].substring(0, charIndex - 1);
+                charIndex--;
+                setTimeout(type, 50);
+            } else {
+                isTyping = true;
+                index = (index + 1) % phrases.length;
+                setTimeout(type, 500); // Pause before typing next phrase
+            }
         }
+    }
+
+    function erase() {
+        isTyping = false;
+        type();
     }
 
     type();
